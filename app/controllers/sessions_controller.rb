d19115b1977c5a6c66b905f0def8cc6ba5bd2619
mayurkhatri <mayurkhatri122@gmail.com>
2018-10-23 16:20:00 +0530

    Populate list of available repository - public and private - github api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
+require 'net/http'
+require 'openssl'
+require 'json'
+
 class SessionsController < ApplicationController
   def new
+    if current_user.present?
+      user_name = current_user.username
+      uri = URI("https://api.github.com/users/#{user_name}/repos?client_id=#{ENV['GITHUB_CLIENT_ID']}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}")
+      repositories = Net::HTTP.get(uri)
+      json_result = JSON.parse(repositories)
+      @repo_names_with_access = json_result.map{|e| e["name"]}
+      # @repo_names_with_access = json_result.map{|e| [e["name"], e["private"], e["avatar_url"]]}
+    end
   end
 
   def create
