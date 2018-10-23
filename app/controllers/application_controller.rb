81558248fc575902eee88ddbc0521f62773daa72
mayurkhatri <mayurkhatri122@gmail.com>
2018-10-23 17:38:05 +0530

    Sessions controller - handle condition for no record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    session[:user_id].nil? ? nil : User.find(session[:user_id])
+    begin
+      session[:user_id].nil? ? nil : User.find(session[:user_id])
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
   end
 end
