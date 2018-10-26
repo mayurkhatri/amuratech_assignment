efca116c64d7aea3aa8b09506416ccf016a7b1ff
mayurkhatri <mayurkhatri122@gmail.com>
2018-10-26 16:21:12 +0530

    fix conflicts

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module AmuratechAssignment
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+      provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user,repo'
   end
 end
